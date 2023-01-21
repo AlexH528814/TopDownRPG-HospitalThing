e45e25c1a8f8f295dd58f697f511f14d6cb70dce
AshenSylvie <showeryear31@gmail.com>
2023-01-21 14:23:01 +0000

    sorted out the score shit sort of, it multiplies the number of items you picked up by 100 and displays that score if you go into the left triangle, you can refresh the items by going into the triangle on the right (technically takes you to an identical scene cus i couldn't be bothered to make it look different, ill do that later)

---
+++ Assets/scripts/final_score.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class final_score : MonoBehaviour
+{
+
+	public TMP_Text score_text;
+
+	void Start()
+	{
+		score_text.SetText($"Your final score is: {application_vars.item_count * 100}");
+	}
+
+	void Update()
+	{
+	}
+}
