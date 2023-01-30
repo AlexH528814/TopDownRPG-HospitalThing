a1c05bfe8eaf153743262a4f2ac0b6b17a44e7a5
AshenSylvie <showeryear31@gmail.com>
2023-01-30 18:40:38 +0000

    UI is completely fucked rn but script for the first patient should be fine

--- Assets/scripts/button_options.cs
+++ Assets/scripts/button_options.cs
@@ -6,32 +6,84 @@ public class button_options : MonoBehaviour
 {
     	public GameObject Canvas;
     	public GameObject face;
-    	public GameObject dialogue;
 
 	public GameObject cold_symptoms;
 	public GameObject pneumonia_symptoms;
 	public GameObject depression_symptoms;
 	public GameObject trauma_symptoms;
 
+	public GameObject patient1_dialogue;
+	public GameObject patient2_dialogue;
+	public GameObject patient3_dialogue;
+	public GameObject patient4_dialogue;
+
+	public GameObject patient1_saved_box;
+	public GameObject patient2_saved_box;
+	public GameObject patient3_saved_box;
+	public GameObject patient4_saved_box;
 
 
+	public GameObject missing_medicine;
 
-    void HealOption()
-	{
-		
+    public IEnumerator PneumoniaOptionP1()
+    {
+	if (application_vars.patient1_radius){
+		if (application_vars.has_pneumonia_medicine){
+			patient1_saved_box.SetActive(true);
+			yield return new WaitForSeconds(3f);
+			LeaveOption();
+			application_vars.patient1_saved = true;
+		}
+
+		else {
+			missing_medicine.SetActive(true);
+			yield return new WaitForSeconds(3f);
+			LeaveOption();
+		}
 	}
+    }
 
-    void Leave_Option()
+    public IEnumerator ColdOptionP1()
+    {
+	if (application_vars.patient1_radius){
+		if (application_vars.has_cold_medicine){
+			patient1_saved_box.SetActive(true);
+			yield return new WaitForSeconds(3f);
+			LeaveOption();
+			application_vars.patient1_saved = true;
+		}
+		else {
+			missing_medicine.SetActive(true);
+			yield return new WaitForSeconds(3f);
+			LeaveOption();
+		}
+	}
+    }
+
+    public void LeaveOption()
     {
         Canvas.SetActive(false);
         face.SetActive(false);
-        dialogue.SetActive(false);
-    
+        patient1_dialogue.SetActive(false);
+	patient2_dialogue.SetActive(false);
+	patient3_dialogue.SetActive(false);
+	patient4_dialogue.SetActive(false);
 
         cold_symptoms.SetActive(false);
         pneumonia_symptoms.SetActive(false);
         depression_symptoms.SetActive(false);
         trauma_symptoms.SetActive(false);
+    }
+
+
+    public void ColdOptionPatient1()
+    {
+	StartCoroutine(ColdOptionP1());
+    }
+
+    public void PneumoniaOptionPatient1()
+    {
+	StartCoroutine(PneumoniaOptionP1());
     }
 
 }
