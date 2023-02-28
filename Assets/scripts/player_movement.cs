10896f3638b51062461431f251eb497c3e5d9d69
AshenSylvie <showeryear31@gmail.com>
2023-02-28 02:23:06 +0000

    added scripts for animation because fk visual scripting

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -4,45 +4,54 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
-    public GameObject Canvas;
 
-    public float move_speed;
-    Rigidbody2D rb;
+	const string FI_IDLE = "fi_idle";
+	const string FI_WALK_UP = "fi_walk_up";
+	const string FI_WALK_DOWN = "fi_walk_down";
+	const string FI_WALK_RIGHT = "fi_walk_right";
+	const string FI_WALK_LEFT = "fi_walk_left";
+	
+	public string lastState;
+    	
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	public GameObject Canvas;
+
+    	public float move_speed;
+    	Rigidbody2D rb;
+    	Animator anim;
+   	// Start is called before the first frame update
+    	void Start()
+    	{
+	anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-    }
+    	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Canvas.activeInHierarchy)
-        {
-            return;
-        }
+    	// Update is called once per frame
+    	void Update()
+    	{
+        	if (Canvas.activeInHierarchy) return;
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+	        float horizontal = Input.GetAxisRaw("Horizontal");
+        	float vertical = Input.GetAxisRaw("Vertical");
 
-        rb.velocity = new Vector2(horizontal * move_speed, vertical * move_speed);
-    }
+        	rb.velocity = new Vector2(horizontal * move_speed, vertical * move_speed);
+    
+		if (vertical < 0 && horizontal == 0) ChangeAnimationState(FI_WALK_DOWN);
+		if (vertical > 0 && horizontal == 0) ChangeAnimationState(FI_WALK_UP);
+		if (horizontal < 0 && vertical == 0) ChangeAnimationState(FI_WALK_LEFT);
+		if (horizontal > 0 && vertical == 0) ChangeAnimationState(FI_WALK_RIGHT);
+		if (vertical < 0 && horizontal < 0) ChangeAnimationState(FI_WALK_DOWN);
+		if (vertical > 0 && horizontal > 0) ChangeAnimationState(FI_WALK_DOWN);
+		if (vertical > 0 && horizontal > 0) ChangeAnimationState(FI_IDLE);
+
+
+	}
+
+    	void ChangeAnimationState(string newState){
+		if (newState == lastState) return;
+    
+		anim.Play(newState);
+
+		lastState = newState;
+    	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
